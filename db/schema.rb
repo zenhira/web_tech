07f9e5f6fc114c41e5933dd31457548d348bed55
yasuhirozenhira <poseidon.0141@gmail.com>
2019-12-12 17:48:49 +0900

    remake_db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_12_035607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
